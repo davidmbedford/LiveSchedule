b400b7b73311693830922e2f6b07dac9c1f2573e
DavidmB <david.m.bedford1@gmail.com>
2019-04-02 12:17:11 -0400

    added more js, still cant figure out submit button issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,39 +14,80 @@
   // Universal Variables
   var database = firebase.database();
 
+$(document).ready(function() {
+
   // Submit Button
   $("#add-train").on("click", function(event) {
-    // Don't refresh the page!
     event.preventDefault();
 
     var name = $("#name-input").val().trim();
     var dest = $("#dest-input").val().trim();
-    var time = $("#time-input").val().trim();
+    var time = moment($("#time-input").val().trim(), "hh:mm").format("hh:mm");
     var freq = $("#freq-input").val().trim();
 
-    database.ref().push({
+    var newTrain = {
       name: name,
       destination: dest,
       time: time,
       frequency: freq
-    });
+      };
 
+    database.ref().push(newTrain);
+
+    alert("Train added!");
+
+    $("#name-input").val("");
+    $("#dest-input").val("");
+    $("#time-input").val("");
+    $("#freq-input").val("");
   });
 
   // Firebase watcher + initial loader HINT: .on("value")
-  database.ref().on("value", function(snapshot) {
+database.ref().on("value", function(snapshot) {
+  event.preventDefault();
+  console.log(snapshot.val());
 
-    // trying to see whats being logged
-    console.log(snapshot.val());
-    console.log(snapshot.val().name);
-    console.log(snapshot.val().destination);
-    console.log(snapshot.val().time);
-    console.log(snapshot.val().frequency);
+    var name = snapshot.val().name;
+    var dest = snapshot.val().destination;
+    var time = snapshot.val().time;
+    var freq = snapshot.val().frequency;
 
-    // this is test code. i will dynamically create divs once i get the firebase portion working
+    console.log(name);
+    console.log(dest);
+    console.log(time);
+    console.log(freq);
 
+    var timeConverted = moment(time, "HH:mm").subtract(1, "years");
+    console.log(timeConverted);
+
+    var currentTime = moment();
+    console.log("current time: " + moment(currentTime).format("hh:mm"));
+
+    var diffTime = moment().diff(moment(timeConverted), "minutes");
+    console.log("Difference in time: " + diffTime);
+
+    var timeRemainder = diffTime % freq;
+    console.log(timeRemainder);
+
+    var tMinsTilTrain = freq - timeRemainder;
+    console.log("Minutes until next train: " + tMinsTilTrain);
+
+    var nextTrain = moment().add(tMinsTilTrain, "minutes");
+    console.log("Arrival time: " + moment(nextTrain).format("hh:mm"));
+
+    var newRow = $("<tr>").append(
+      $("<td>").text(name),
+      $("<td>").text(dest),
+      $("<td>").text(freq),
+      $("<td>").text(nextTrain),
+      $("<td>").text(tMinsTilTrain)
+    );
+
+    $("#train-table > tbody").append(newRow);
 
     // Handle the errors. ask ta about other database failing
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
+
+});
