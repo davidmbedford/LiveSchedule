70100cb8b407292381f2755ae7ebfe4661e7f3fb
DavidmB <david.m.bedford1@gmail.com>
2019-04-01 22:25:04 -0400

    framework done

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAZuSMKcBu-lzKLuBNY0M8Eve_CpthUKls",
+    authDomain: "unit-7-train-schedule.firebaseapp.com",
+    databaseURL: "https://unit-7-train-schedule.firebaseio.com",
+    projectId: "unit-7-train-schedule",
+    storageBucket: "unit-7-train-schedule.appspot.com",
+    messagingSenderId: "124735987706"
+  };
+
+  firebase.initializeApp(config);
+
+  // Universal Variables
+  var
